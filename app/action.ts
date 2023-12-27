6637b644a1356fc44caff778f163f36e371bf002
Ivan Pajon <ivangpajon@hotmail.com>
2023-12-27 02:09:55 +0100

    Workaround for proper behavior

--- app/action.ts
+++ app/action.ts
@@ -1,8 +1,9 @@
 'use server';
 
 export async function logForm(form: FormData) {
-  // This FormData SHOULD have an entry with id and name 'fixedName'
-  console.log(form.get('fileUploadFixedName'));
-  console.log(form.get('fixedName'));
-  console.log(form.get('someDynamicName'));
+  // console.log(form.get('fileUploadFixedName'));
+  // console.log(form.get('fixedName'));
+  // console.log(form.get('someDynamicName'));
+  console.log(form.get('wrongFile'));
+  console.log(form.get('correctFile'));
 }
--- app/component.tsx
+++ app/component.tsx
@@ -1,18 +1,45 @@
 'use client';
 
 import { Button } from 'primereact/button';
-import { FileUpload } from 'primereact/fileupload';
+import { FileUpload, FileUploadSelectEvent } from 'primereact/fileupload';
+import { useRef } from 'react';
 import { logForm } from './action';
 
+/**
+ * Bug reproduction of FileUpload
+ * 
+ * For proper behavior, I have to implement the following solution:
+ * https://stackoverflow.com/a/76108735
+ *
+ * @return {*} Form with FileUpload examples
+ */
 export function FormFileUpload() {
-  const dynamicName = 'someDynamicName';
+	const inputRef = useRef<HTMLInputElement>(null);
+
+  const dynamicName = 'someDynamicName'; // Dynamic input name
+
+	const onTemplateSelect = (event: FileUploadSelectEvent) => {
+    const dataTransfer = new DataTransfer();
+
+    for (const file of event.files) {
+			dataTransfer.items.add(file);
+    }
+
+		inputRef.current!.files = dataTransfer.files;
+  };
 
   return (
 		<form action={logForm} className="card flex flex-col items-center gap-6 mt-4">
-      {/* This form never send file */}
-			<FileUpload id="fileUpload" name="fileUpload" mode="advanced" accept="image/*" maxFileSize={1000000} />
+      {/* This FileUpload never send file */}
+			<FileUpload
+				id="fileUpload"
+				name="fileUpload"
+				mode="advanced"
+				accept="image/*"
+				maxFileSize={1000000}
+			/>
 			
-      {/* This form send file in fixedName instead of fileUploadFixedName */}
+      {/* This FileUpload send wrong file in fixedName instead of fileUploadFixedName */}
       <FileUpload
 				id="fileUploadFixedName"
 				name="fileUploadFixedName"
@@ -24,7 +51,7 @@ export function FormFileUpload() {
 				}}
 			/>
 
-      {/* This form send file with value key of dynamicName constant instead of fileUploadDynamicName */}
+      {/* This FileUpload send wrong file with key value of dynamicName constant instead of fileUploadDynamicName */}
 			<FileUpload
 				id="fileUploadDynamicName"
 				name="fileUploadDynamicName"
@@ -33,6 +60,19 @@ export function FormFileUpload() {
 				maxFileSize={1000000}
 				pt={{
 					input: { name: dynamicName },
+				}}
+			/>
+
+      {/* This input send correct file in correctFile setted by wrongFile FileUpload */}
+			<input ref={inputRef} name="correctFile" type="file" className="hidden" />
+      {/* This FileUpload send wrong file in wrongFile, but set the correct file in correctFile */}
+			<FileUpload
+				mode="advanced"
+				accept="image/*"
+				maxFileSize={1000000}
+				onSelect={onTemplateSelect}
+				pt={{
+					input: { name: 'wrongFile' },
 				}}
 			/>
 
